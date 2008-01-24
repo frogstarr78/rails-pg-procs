18c5cd6dfdcbbc242bb37028619a9c86b3366b5f
frogstarr78 <frogstarr78@c8896819-c966-4d5c-9ea0-1c0573f89364>
2008-01-24 04:52:51 +0000

    Adding Alpha implementation.
    git-svn-id: http://railspgprocs.rubyforge.org/svn@1 c8896819-c966-4d5c-9ea0-1c0573f89364

---
+++ lib/rails_pg_procs.rb
@@ -0,0 +1,268 @@
+gem 'activesupport'
+gem 'activerecord'
+require 'active_record'
+require "active_support/inflector"
+
+module Inflector
+  def triggerize(table_name, events=[], before=false)
+    events.join(" or ").sub(":", "").tr(" ", "_").downcase + "_" + (before ? "before_" : "after_") + table_name.to_s + "_trigger"
+  end
+
+  def symbolize(val)
+    return "'#{val}'" if val =~ /-/
+    ":#{val}"
+  end
+end
+
+# RailsPgProcs
+module ActiveRecord
+  # This class is used to dump the database schema for some connection to some
+  # output format (i.e., ActiveRecord::Schema).
+  class SchemaDumper
+    ROW    = 0b00001
+    BEFORE = 0b00010
+    INSERT = 0b00100
+    DELETE = 0b01000
+    UPDATE = 0b10000
+
+    # TODO -Implement checks on SchemaDumper instance 
+    # to ensure we do this only when using pg db.
+    def postgres?
+      adapter_name == 'PostgreSQL'
+    end
+    private
+      def get_type(types)
+        case types
+          when Array
+            types.collect {|type|
+              get_type(type)
+            }.join(", ")
+          when String && /^\d+$/
+            type = @connection.select_value("SELECT typname FROM pg_type WHERE oid = '#{types}'")
+            return type = 'nil' if type == 'void'
+            get_type(type)
+          when String
+            return %("#{types}") if types =~ /[\s\(]/
+            Inflector.symbolize(types)
+        end
+      end
+
+      def procedures(stream)
+        @connection.procedures().each {|proc_row|
+          oid, name, namespace, owner, lang, is_agg, sec_def, is_strict, ret_set, volatile, nargs, ret_type, arg_types, arg_names, src, bin, acl = proc_row
+          is_agg    = is_agg    == 't'
+          is_strict = is_strict == 't'
+          ret_set   = ret_set   == 't'
+          volatile  = %w{immutable stable}.grep(/^#{volatile}.+/).to_s unless volatile == 'v'
+          lang      = @connection.select_value("SELECT lanname FROM pg_language WHERE oid = '#{lang}'")
+          arg_names ||= ''
+          args      = get_type(arg_types.split(" "))#.zip(arg_names.split(" "))
+
+          stream.print "  create_proc(#{Inflector.symbolize(name)}, [#{args}], :return => #{get_type(ret_type)}"
+          stream.print ", :resource => ['#{bin}', '#{src}']" unless bin == '-'
+          stream.print ", :set => true" if ret_set
+          stream.print ", :strict => true" if is_strict
+          stream.print ", :behavior => '#{volatile}'" unless volatile == 'v'
+          stream.print ", :lang => '#{lang}')"
+          stream.print " {\n<<-#{Inflector.underscore(name)}_sql\n#{src.chomp}\n#{Inflector.underscore(name)}_sql\n}" if bin == '-'
+          stream.print "\n"
+        }
+      end
+      def triggers(table_name, stream)
+        triggers = @connection.triggers(table_name)
+        unless triggers.empty?
+          triggers.each {|trigger|
+            stream.print "  add_trigger(#{Inflector.symbolize(table_name)}" 
+           events = []
+            events.push(":insert") if calc(trigger.type, INSERT)
+            events.push(":update") if calc(trigger.type, UPDATE)
+            events.push(":delete") if calc(trigger.type, DELETE)
+
+            stream.print ", [" + events.join(", ") + "]"
+            stream.print ", :before => true" if calc(trigger.type, BEFORE)
+            stream.print ", :row => true" if calc(trigger.type, ROW)
+            stream.print ", :name => #{Inflector.symbolize(trigger.name)}" if Inflector.triggerize(table_name, events, calc(trigger.type, BEFORE)) != trigger.name
+            stream.print ", :function => #{Inflector.symbolize(trigger.procedure_name)}" if Inflector.triggerize(table_name, events, calc(trigger.type, BEFORE)) != trigger.name
+            stream.puts ")"
+          }
+        end
+        stream.puts unless triggers.empty?
+      end
+
+      def types(stream)
+        @connection.types.each {|type|
+          stream.print "  create_type(#{Inflector.symbolize(type.name)}, ["
+          stream.print "#{ type.columns.collect{|column, type| "[#{Inflector.symbolize(column)}, #{get_type(type)}]"}.join(", ") }"
+          stream.puts  "])"
+        }
+      end
+
+      alias_method :procless_tables, :tables
+      def tables(stream)
+        types(stream)
+        procedures(stream)
+        procless_tables(stream)
+      end
+
+      alias_method :indexes_before_triggers, :indexes
+      def indexes(table, stream)
+        indexes_before_triggers(table, stream)
+        triggers(table, stream)
+      end
+
+      def calc(int, bin)
+        eval(sprintf("0b%0.8b", int)) & bin > 0
+      end
+  end
+
+  module ConnectionAdapters
+
+    class TriggerDefinition < Struct.new(:id, :name, :type, :procedure_name); end
+    class TypeDefinition < Struct.new(:id, :name, :columns); end
+
+    class PostgreSQLAdapter < AbstractAdapter
+      cattr_accessor :first_proc_oid
+#      @@first_proc_oid = 10634
+      @@first_proc_oid = "(SELECT (MAX(oid::int)-MIN(oid::int))/2 FROM pg_proc)"
+      def procedures
+        query <<-end_sql
+          SELECT oid, proname, pronamespace, proowner, prolang, proisagg, prosecdef, proisstrict, proretset, provolatile, pronargs, prorettype, proargtypes, proargnames, prosrc, probin, proacl
+            FROM pg_proc
+           WHERE oid > #{self.class.first_proc_oid}
+        end_sql
+      end
+
+      def triggers(table_name)
+        query(<<-end_sql).collect {|row| TriggerDefinition.new(*row) }
+          SELECT T.oid, T.tgname, T.tgtype, P.proname
+            FROM pg_trigger T
+            JOIN pg_class   C ON (T.tgrelid = C.OID AND C.relname = '#{table_name}' AND T.tgisconstraint = 'f')
+            JOIN pg_proc    P ON (T.tgfoid = P.OID)
+        end_sql
+      end
+
+      def types
+        result = query(<<-end_sql)
+          SELECT T.oid, T.typname, A.attname, format_type(A.atttypid, A.atttypmod) AS type
+            FROM pg_type      T
+            JOIN pg_class     C ON (T.typrelid = C.oid)
+            JOIN pg_attribute A ON (A.attrelid = C.oid AND C.relkind = 'c')
+        end_sql
+
+        type_id = nil
+        types = []
+        result.each { |row|
+          if type_id != row[0]
+            types << TypeDefinition.new(row[0], row[1], [])
+            type_id = row[0]
+          end
+
+          types.last.columns << [row[2], row[3]]
+        }
+
+        types
+      end
+
+      def create_type(name, columns)
+        execute get_type_query(name, columns)
+      end
+      
+      def drop_type(name, drop_dependants="RESTRICT")
+        execute "DROP TYPE #{name} #{drop_dependants.to_s.upcase}"
+      end
+
+      def add_trigger(table, events, options={})
+        execute get_trigger_query(table, events, options)
+      end
+
+#      DROP TRIGGER name ON table [ CASCADE | RESTRICT ]
+      def remove_trigger(table, name, options={})
+        execute "DROP TRIGGER #{name} ON #{table} #{options[:deep] || 'RESTRICT'};"
+      end
+
+      def create_proc(name, columns=[], options={}, &block)
+        if select_value("SELECT count(oid) FROM pg_language WHERE lanname = 'plpgsql' ","count").to_i == 0
+          execute("CREATE TRUSTED PROCEDURAL LANGUAGE plpgsql HANDLER plpgsql_call_handler")
+        end
+
+        if block_given?
+          execute get_proc_query(name, columns, options) { yield }
+        elsif options[:resource]
+          execute get_proc_query(name, columns, options)
+        else
+          raise StatementInvalid.new
+        end
+      end
+
+#      DROP FUNCTION name ( [ type [, ...] ] ) [ CASCADE | RESTRICT ]
+      def drop_proc(name, columns=[], options={})
+        execute "DROP FUNCTION \"#{name}\"(#{columns.collect {|column| quote_column_name(column)}.join(", ")}) #{options[:deep] || 'RESTRICT'};"
+      end
+
+      private 
+#       CREATE TRIGGER name { BEFORE | AFTER } { event [ OR ... ] }
+#       ON table [ FOR [ EACH ] { ROW | STATEMENT } ]
+#       EXECUTE PROCEDURE funcname ( arguments )
+        def get_trigger_query(table, events, options={})
+          event_str = events.collect {|event| event.to_s.upcase }.join(" OR ")
+          trigger_name = options[:name] || Inflector.triggerize(table, events, options[:before])
+          func_name = options[:function] || trigger_name
+          result = "CREATE TRIGGER #{trigger_name} #{(options[:before] ? "BEFORE" : "AFTER")} #{event_str} ON #{table} FOR EACH #{(options[:row] ? "ROW" : "STATEMENT")} EXECUTE PROCEDURE #{func_name}();"
+        end
+
+##      CREATE [ OR REPLACE ] FUNCTION
+##          name ( [ [ argmode ] [ argname ] argtype [, ...] ] )
+##          [ RETURNS rettype ]
+##        { LANGUAGE langname
+##          | IMMUTABLE | STABLE | VOLATILE
+##          | CALLED ON NULL INPUT | RETURNS NULL ON NULL INPUT | STRICT
+#          | [ EXTERNAL ] SECURITY INVOKER | [ EXTERNAL ] SECURITY DEFINER
+##          | AS 'definition'
+#          | AS 'obj_file', 'link_symbol'
+#        } ...
+#          [ WITH ( isStrict &| isCacheable ) ]
+        def get_proc_query(name, columns=[], options={}, &block)
+          # TODO
+          # [ [ argmode ] [ argname ] argtype ]
+          returns = ''
+          if options.has_key?(:return)
+            returns = "RETURNS#{' SETOF' if options[:set]} #{options[:return] || 'VOID'}"
+          end
+          lang = options[:lang] || "plpgsql"
+
+          if block_given?
+            body = "$#{Inflector.underscore(name)}_body$
+#{yield}
+$#{Inflector.underscore(name)}_body$"
+          elsif options[:resource]
+            options[:resource] += [name] if options[:resource].size == 1
+            body = options[:resource].collect {|res| "'#{res}'" }.join(", ")
+          else
+            raise StatementInvalid.new and return
+          end
+
+          result = "CREATE OR REPLACE FUNCTION \"#{name}\"(#{columns.collect{|column| quote_column_name(column)}.join(", ")}) #{returns} AS
+#{body}
+LANGUAGE #{lang}
+#{ (options[:behavior] || 'VOLATILE').upcase }
+#{ options[:strict] ? 'STRICT' : 'CALLED ON NULL INPUT'}
+EXTERNAL SECURITY #{ options[:user] == 'definer' ? 'DEFINER' : 'INVOKER' }
+"
+        end
+
+        def get_type_query(name, columns)
+          raise StatementInvalid.new if !columns.is_a?(Array) or columns.empty?
+          "CREATE TYPE #{quote_column_name(name)} AS (
+            #{columns.collect{|column, type|
+              if column.is_a?(Hash)
+                column.collect { |column, type| "#{quote_column_name(column.to_s)} #{type}" }
+              else
+              "#{quote_column_name(column.to_s)} #{type}"
+              end
+              
+            }.join(",\n")}
+          )"
+        end
+    end
+  end
+end
---
+++ test/rails_pg_procs_test.rb
@@ -0,0 +1,316 @@
+require 'test/unit'
+require 'stringio'
+require 'runit/assert'
+require File.dirname(__FILE__) + '/../lib/rails_pg_procs'
+require File.dirname(__FILE__) + '/connection'
+
+class String
+  def to_regex
+    Regexp.new(self.tr(' ', "\\\s").gsub(/([\(\)\[\]\{\}\.\\\$])/) {|s| '\\' + s })
+  end
+end
+
+
+class ActiveRecord::SchemaDumper; public_class_method :new; end
+# we want new public for the tests so we can test each method independantly from the rest
+
+class RailsPgProcsTest < Test::Unit::TestCase
+#  include ActiveRecord::ConnectionAdapters::SchemaStatements
+  @_use_transactional_fixtures = false
+
+  include RUNIT::Assert
+  def setup
+    super
+    @connection = ActiveRecord::Base.connection
+    @connection.create_table(:test_table, :force => true) {|t|
+      t.column :name, :text
+      t.column :when, :datetime
+    }
+
+    @query_body = "
+  BEGIN
+        -- do something --
+  END;
+"
+  end
+
+  def teardown
+    @connection.drop_table(:test_table)
+  end
+
+  def test_constants
+    assert_equal 1<<0, ActiveRecord::SchemaDumper::ROW
+    assert_equal 1<<1, ActiveRecord::SchemaDumper::BEFORE
+    assert_equal 1<<2, ActiveRecord::SchemaDumper::INSERT
+    assert_equal 1<<3, ActiveRecord::SchemaDumper::DELETE
+    assert_equal 1<<4, ActiveRecord::SchemaDumper::UPDATE
+  end
+
+  def test_methods
+    %w(add_trigger remove_trigger create_proc drop_proc procedures triggers types).each {|meth|
+      assert_respond_to meth, @connection
+    }
+
+    assert !@connection.procedures().nil?, "@connection#procedures returns nil"
+    @connection.drop_proc(:insert_after_test_table_trigger) if "insert_after_test_table_trigger" == @connection.procedures.result.last[1]
+    procedures_count = @connection.procedures().result.size
+    trigger_count = @connection.triggers(:test_table).size
+    with_proc(:insert_after_test_table_trigger, [], :return => :trigger) {
+      assert_equal 0, trigger_count
+      assert_equal procedures_count + 1, @connection.procedures().result.size
+      with_trigger(:test_table, [:insert], :row => true) {
+        assert !@connection.triggers(:test_table).nil?, "Triggers for table :test_table returns nil"
+        received = @connection.triggers(:test_table)
+        assert_equal trigger_count + 1, received.size
+        assert_equal "insert_after_test_table_trigger", received.last[1]
+      }
+    }
+    assert_equal procedures_count, @connection.procedures().result.size
+  end
+
+  def test_functional_dump
+    @connection.create_type(:qualitysmith_user, [[:name, :varchar], {:address => "varchar(20)"}, [:zip, "varchar(5)"], [:phone, "numeric(10,0)"]])
+    @connection.create_proc("name-with-hyphen", [], :return => :trigger) { "  BEGIN\n--Something else goes here\nEND;\n" }
+    @connection.create_proc(:update_trade_materials_statuses_logf, [], :return => :trigger) { "  BEGIN\n--Something else goes here\nEND;\n" }
+    @connection.create_proc(:levenshtein, [], :return => :trigger, :resource => ['$libdir/fuzzystrmatch'], :lang => "c")
+    @connection.add_trigger(:test_table, [:insert, :update], :row => true, :name => :update_trade_materials_statuses_logt, :function => :update_trade_materials_statuses_logf)
+    @connection.add_trigger(:test_table, [:insert, :update], :function => :levenshtein)
+
+    stream = StringIO.new
+    ActiveRecord::SchemaDumper.ignore_tables = []
+    ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
+    received_sql = stream.string
+
+    f = File.open("out.rb", "w")
+    f.write received_sql
+    f.close
+    assert_match('create_type(:qualitysmith_user, [[:name, "character varying"], [:address, "character varying(20)"], [:zip, "character varying(5)"], [:phone, "numeric(10,0)"]])'.to_regex, received_sql)
+    assert_match("create_proc('name-with-hyphen'".to_regex, received_sql)
+    assert_match('create_proc(:update_trade_materials_statuses_logf'.to_regex, received_sql)
+    assert_match('create_proc(:levenshtein, []'.to_regex, received_sql)
+    assert_match('add_trigger(:test_table, [:insert, :update], :name => :insert_or_update_after_test_table_trigger, :function => :levenshtein)'.to_regex, received_sql)
+    assert_match('add_trigger(:test_table, [:insert, :update], :row => true, :name => :update_trade_materials_statuses_logt, :function => :update_trade_materials_statuses_logf)'.to_regex, received_sql)
+
+    @connection.remove_trigger(:test_table, :update_trade_materials_statuses_logt)
+    @connection.remove_trigger(:test_table, :insert_or_update_after_test_table_trigger)
+    @connection.drop_proc(:update_trade_materials_statuses_logf)
+    @connection.drop_proc(:levenshtein)
+    @connection.drop_proc('name-with-hyphen')
+    @connection.drop_type(:qualitysmith_user)
+  end
+
+  def test_schema_dumper
+    # a simple test
+    with_proc(:insert_after_test_table_trigger, [], :return => :trigger) {
+      with_trigger(:test_table, [:insert], :row => true) {
+        assert_no_exception(NoMethodError) do 
+          stream = StringIO.new
+          dumper = ActiveRecord::SchemaDumper.new(@connection)
+          dumper.send(:triggers, :test_table, stream)
+          stream.rewind
+          assert_equal "  add_trigger(:test_table, [:insert], :row => true)\n\n", stream.read
+
+          stream = StringIO.new
+          dumper.send(:procedures, stream)
+          stream.rewind
+          assert_match "  create_proc(:insert_after_test_table_trigger, [], :return => :trigger, :lang => 'plpgsql') {\n    <<-insert_after_test_table_trigger_sql\n\n#{@query_body}\n	  insert_after_test_table_trigger_sql\n	}\n".to_regex, stream.read
+        end
+      }
+    }
+
+    # a more complicated test
+    with_proc(:update_trade_materials_statuses_logf, [], :return => :trigger) {
+      with_trigger(:test_table, [:insert, :update], :before => true, :name => :update_trade_materials_statuses_logt, :function => :update_trade_materials_statuses_logf) {
+        assert_no_exception(NoMethodError) do 
+          stream = StringIO.new
+          dumper = ActiveRecord::SchemaDumper.new(@connection)
+          dumper.send(:triggers, :test_table, stream)
+          stream.rewind
+          assert_equal "  add_trigger(:test_table, [:insert, :update], :before => true, :name => :update_trade_materials_statuses_logt, :function => :update_trade_materials_statuses_logf)\n\n", stream.read
+
+          stream = StringIO.new
+          dumper.send(:procedures, stream)
+          stream.rewind
+          received_sql = stream.string
+          assert_match "  create_proc(:update_trade_materials_statuses_logf, [], :return => :trigger, :lang => 'plpgsql') {\n    <<-update_trade_materials_statuses_logf_sql\n\n#{@query_body}\n    update_trade_materials_statuses_logf_sql\n  }\n".to_regex, received_sql
+        end
+      }
+    }
+
+    # a more-more complicated test
+    with_proc(:levenshtein, [:text, :text], :return => nil, :resource => ['$libdir/fuzzystrmatch'], :strict => true, :behavior => 'immutable', :lang => "C") {
+      assert_no_exception(NoMethodError) do 
+        dumper = ActiveRecord::SchemaDumper.new(@connection)
+        stream = StringIO.new
+        dumper.send(:procedures, stream)
+        stream.rewind
+        received = stream.read
+        assert_equal "  create_proc(:levenshtein, [:text, :text], :return => nil, :resource => ['$libdir/fuzzystrmatch', 'levenshtein'], :strict => true, :behavior => 'immutable', :lang => 'c')", received.split("\n")[-1]
+      end
+    }
+
+    # a type test
+    @connection.create_type(:qualitysmith_user, [[:name, "varchar(10)"], {:zip => "numeric(5,0)"}, [:is_customer => :boolean]])
+    assert_no_exception(NoMethodError) do 
+      dumper = ActiveRecord::SchemaDumper.new(@connection)
+      stream = StringIO.new
+      dumper.send(:types, stream)
+      stream.rewind
+      received = stream.read
+       assert_equal '  create_type(:qualitysmith_user, [[:name, "character varying(10)"], [:zip, "numeric(5,0)"], [:is_customer, :boolean]])', received.chomp
+    end
+    @connection.drop_type(:qualitysmith_user)
+  end
+
+  def test_calculations
+    dumper = ActiveRecord::SchemaDumper.new(@connection)
+    assert dumper.send("calc", 0b00010101, ActiveRecord::SchemaDumper::INSERT)
+    assert dumper.send("calc", 0b00010101, ActiveRecord::SchemaDumper::UPDATE)
+    assert dumper.send("calc", 0b00010101, ActiveRecord::SchemaDumper::ROW)
+    assert dumper.send("calc", 0b00001010, ActiveRecord::SchemaDumper::DELETE)
+    assert dumper.send("calc", 0b00001010, ActiveRecord::SchemaDumper::BEFORE)
+  end
+
+  def test_create_type()
+    [
+      'CREATE TYPE "user"',
+      '"name" varchar(10)',
+      '"zip" numeric(5,0)'
+    ].each {|re|
+      assert_match re.to_regex, @connection.send("get_type_query", "user", [[:name, "varchar(10)"], {:zip => "numeric(5,0)"}, [:is_customer => :boolean]])
+    }
+
+    assert_exception(ActiveRecord::StatementInvalid, "Missing columns") {
+      @connection.create_type("user", {})
+    }
+    assert_exception(ActiveRecord::StatementInvalid, "Missing columns") {
+      @connection.create_type("user", [])
+    }
+
+    assert_nothing_raised {
+      @connection.create_type(:qualitysmith_user, [[:name, "varchar(10)"], {:zip => "numeric(5,0)"}, [:is_customer => :boolean]])
+    }
+    count = @connection.select_value("select count(*) from pg_type where typname = 'qualitysmith_user'", "count")
+    assert_equal("1", count)
+    assert_nothing_raised {
+      @connection.drop_type(:qualitysmith_user)
+    }
+    count = @connection.select_value("select count(*) from pg_type where typname = 'qualitysmith_user'", "count")
+    assert_equal("0", count)
+  end
+
+  def test_create_proc()
+    [
+      /logf()/,
+      /\'resource\/file\',\s\'method\'/,
+      /LANGUAGE C/
+    ].each {|re| 
+      assert_match(re, @connection.send("get_proc_query", "logf", [], :return => nil, :resource => ['resource/file', 'method'], :lang => "C"))
+    }
+
+    [
+      /logf()/,
+      /\'resource\/file\',\s\'logf\'/,
+      /LANGUAGE C/
+    ].each {|re| 
+      assert_match(re, @connection.send("get_proc_query", "logf", [], :return => nil, :resource => ['resource/file'], :lang => "C"))
+    }
+
+    [
+      /CREATE OR REPLACE FUNCTION "logf"()/,
+      /RETURNS VOID/,
+      /\$logf_body\$/,
+      @query_body.to_regex,
+      /LANGUAGE plpgsql/,
+      /IMMUTABLE/,
+      /STRICT/,
+      /EXTERNAL SECURITY DEFINER/
+    ].each {|re| 
+      assert_match(re, @connection.send("get_proc_query", "logf", [], :return => nil, :user => 'definer', :strict => true, :behavior => 'immutable') { @query_body })
+    }
+
+    [
+      /update_trade_materials_statuses_logf()/,
+      /RETURNS VOID/,
+      /\$update_trade_materials_statuses_logf_body\$/,
+      @query_body.to_regex,
+      /LANGUAGE SQL/,
+    ].each {|re| 
+      assert_match(re, @connection.send("get_proc_query", "update_trade_materials_statuses_logf", [], :return => nil, :lang => :SQL) { @query_body })
+    }
+
+    [
+      /update_trade_materials_statuses_logf()/,
+      /RETURNS trigger/,
+      /\$update_trade_materials_statuses_logf_body\$/,
+      @query_body.to_regex,
+      /LANGUAGE plpgsql/,
+      /VOLATILE/,
+      /CALLED ON NULL INPUT/,
+      /EXTERNAL SECURITY INVOKER/
+    ].each {|re| 
+      assert_match(re, @connection.send("get_proc_query", "update_trade_materials_statuses_logf", [], :return => "trigger") { @query_body })
+    }
+
+    assert_exception(ActiveRecord::StatementInvalid, "Missing block or library file.") {
+      @connection.create_proc("update_trade_materials_statuses_logf", [], :return => "trigger")
+    }
+
+    assert_nothing_raised {
+      @connection.create_proc(:update_trade_materials_statuses_logf, [], :return => :trigger) {
+        @query_body
+      }
+    }
+    count = @connection.select_value("select count(*) from pg_proc where proname = 'update_trade_materials_statuses_logf'", "count")
+    assert_equal("1", count)
+    assert_nothing_raised {
+      @connection.drop_proc("update_trade_materials_statuses_logf", [])
+    }
+    count = @connection.select_value("select count(*) from pg_proc where proname = 'update_trade_materials_statuses_logf'", "count")
+    assert_equal("0", count)
+  end
+
+  def test_add_trigger
+    assert_equal("CREATE TRIGGER insert_or_update_after_trade_materials_trigger AFTER INSERT OR UPDATE ON trade_materials FOR EACH STATEMENT EXECUTE PROCEDURE insert_or_update_after_trade_materials_trigger();", \
+      @connection.send("get_trigger_query", "trade_materials", [:insert, :update])
+    )
+    assert_equal("CREATE TRIGGER update_trade_materials_statuses_logt AFTER INSERT OR UPDATE ON trade_materials FOR EACH STATEMENT EXECUTE PROCEDURE update_trade_materials_statuses_logt();", \
+      @connection.send("get_trigger_query", "trade_materials", [:insert, :update], :name => "update_trade_materials_statuses_logt")
+    )
+    assert_equal("CREATE TRIGGER update_trade_materials_statuses_logt BEFORE INSERT OR UPDATE ON trade_materials FOR EACH ROW EXECUTE PROCEDURE update_trade_materials_statuses_logf();", \
+      @connection.send("get_trigger_query", "trade_materials", [:insert, :update], :before => true, :row => true, :name => "update_trade_materials_statuses_logt", :function => "update_trade_materials_statuses_logf")
+    )
+
+    @connection.create_proc("update_trade_materials_statuses_logf", [], :return => "trigger") { @query_body }
+    assert_nothing_raised {
+      @connection.add_trigger("test_table", [:insert, :update], :row => true, :name => "update_trade_materials_statuses_logt", :function => "update_trade_materials_statuses_logf") 
+    }
+    count = @connection.select_value("SELECT count(*) FROM pg_trigger WHERE tgname = 'update_trade_materials_statuses_logt' and tgrelid = (SELECT oid FROM pg_class WHERE relname = 'test_table')", "count")
+    assert_equal("1", count)
+    assert_nothing_raised {
+      @connection.remove_trigger("test_table", "update_trade_materials_statuses_logt")
+    }
+    count = @connection.select_value("SELECT count(*) FROM pg_trigger WHERE tgname = 'update_trade_materials_statuses_logt' and tgrelid = (SELECT oid FROM pg_class WHERE relname = 'test_table')", "count")
+    assert_equal("0", count)
+    @connection.drop_proc("update_trade_materials_statuses_logf", [])
+  end
+
+  def with_proc(name, columns=[], options={}, &block)
+    assert_not_equal name, @connection.procedures.result.last[1]
+    if options[:resource]
+      @connection.create_proc(name, columns, options)
+    else
+      @connection.create_proc(name, columns, options) { @query_body }
+    end
+    assert_equal name.to_s, @connection.procedures.result.last[1]
+      yield
+    @connection.drop_proc(name, columns)
+    assert_not_equal name.to_s, @connection.procedures.result.last[1]
+  end
+
+  def with_trigger(table, columns=[], options={}, &block)
+    @connection.add_trigger(table, columns, options) 
+      yield
+    @connection.remove_trigger(table, options[:name] || Inflector.triggerize(table, columns, options.has_key?(:before)))
+  end
+end
